8e05a062d13477f950f7ff127b15c2e787a1fe80
GBNUM <exborgcyborg@gmail.com>
2025-02-12 14:49:41 +0800

    save changes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clear-cache', function() {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    return "Cache cleared!";
+});
